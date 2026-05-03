76772f827c7d780c0b8f8b41af12b2bc2615aae5
Salman Mohammed <wolverinemds7@gmail.com>
2026-05-03 17:12:49 +0530

    Restructuring of Code, Folder Structure

--- go.mod
+++ go.mod
@@ -3,30 +3,30 @@ module github.com/Lucifer-Stone/jlens
 go 1.26.2
 
 require (
+	github.com/atotto/clipboard v0.1.4
 	github.com/fatih/color v1.19.0
+	github.com/gdamore/tcell/v2 v2.13.9
 	github.com/mattn/go-isatty v0.0.21
+	github.com/pelletier/go-toml/v2 v2.3.1
+	github.com/rivo/tview v0.42.0
 	github.com/spf13/cobra v1.10.2
 	github.com/tidwall/gjson v1.18.0
+	github.com/xeipuuv/gojsonschema v1.2.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/gdamore/encoding v1.0.1 // indirect
-	github.com/gdamore/tcell/v2 v2.13.9 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/pelletier/go-toml/v2 v2.3.1 // indirect
-	github.com/rivo/tview v0.42.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/term v0.37.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,14 +6,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"reflect"
 	"regexp"
 	"sort"
 	"strings"
 
+	"github.com/atotto/clipboard"
 	"github.com/fatih/color"
+	"github.com/gdamore/tcell/v2"
 	"github.com/mattn/go-isatty"
+	"github.com/pelletier/go-toml/v2"
+	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 	"github.com/tidwall/gjson"
+	"github.com/xeipuuv/gojsonschema"
+	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -377,4 +384,378 @@ func printStats(obj interface{}) {
 	fmt.Printf("Total Arrays:  %d\n", totalArrays)
 	fmt.Printf("Total Keys:    %d\n", totalKeys)
 	fmt.Printf("Max Depth:     %d\n", maxDepth)
+}
+
+// --- Diff Engine ---
+
+type DiffType int
+
+const (
+	DiffAdd DiffType = iota
+	DiffRemove
+	DiffModify
+)
+
+type DiffResult struct {
+	Type     DiffType
+	Path     string
+	OldValue interface{}
+	NewValue interface{}
+}
+
+func runDiff(data1 []byte, file2 string) error {
+	data2, err := os.ReadFile(file2)
+	if err != nil {
+		return fmt.Errorf("failed to read diff file: %w", err)
+	}
+
+	var obj1, obj2 interface{}
+	if err := json.Unmarshal(data1, &obj1); err != nil {
+		return fmt.Errorf("failed to parse input JSON: %w", err)
+	}
+	if err := json.Unmarshal(data2, &obj2); err != nil {
+		return fmt.Errorf("failed to parse target diff JSON: %w", err)
+	}
+
+	diffs := compareJSON("root", obj1, obj2)
+
+	if len(diffs) == 0 {
+		fmt.Println(color.GreenString("✔ JSON payloads are structurally identical."))
+		return nil
+	}
+
+	fmt.Println(color.YellowString("⚠ Differences detected:\n"))
+
+	for _, d := range diffs {
+		pathStr := color.CyanString(d.Path)
+		switch d.Type {
+		case DiffModify:
+			fmt.Printf("[~] %s: %s\n", color.YellowString("MODIFIED"), pathStr)
+			oldStr, _ := formatJSONFast(d.OldValue)
+			newStr, _ := formatJSONFast(d.NewValue)
+			fmt.Printf("    %s\n", color.RedString("- "+oldStr))
+			fmt.Printf("    %s\n", color.GreenString("+ "+newStr))
+		case DiffAdd:
+			fmt.Printf("[+] %s: %s\n", color.GreenString("ADDED   "), pathStr)
+			newStr, _ := formatJSONFast(d.NewValue)
+			fmt.Printf("    %s\n", color.GreenString("+ "+newStr))
+		case DiffRemove:
+			fmt.Printf("[-] %s: %s\n", color.RedString("DELETED "), pathStr)
+			oldStr, _ := formatJSONFast(d.OldValue)
+			fmt.Printf("    %s\n", color.RedString("- "+oldStr))
+		}
+		fmt.Println()
+	}
+
+	return nil
+}
+
+func compareJSON(path string, a, b interface{}) []DiffResult {
+	var results []DiffResult
+
+	if a == nil && b == nil {
+		return results
+	}
+	if a == nil {
+		return []DiffResult{{Type: DiffAdd, Path: path, NewValue: b}}
+	}
+	if b == nil {
+		return []DiffResult{{Type: DiffRemove, Path: path, OldValue: a}}
+	}
+
+	valA := reflect.ValueOf(a)
+	valB := reflect.ValueOf(b)
+
+	if valA.Type() != valB.Type() {
+		return []DiffResult{{Type: DiffModify, Path: path, OldValue: a, NewValue: b}}
+	}
+
+	switch valA.Kind() {
+	case reflect.Map:
+		mapA := a.(map[string]interface{})
+		mapB := b.(map[string]interface{})
+
+		keysA := getKeys(mapA)
+		keysB := getKeys(mapB)
+
+		allKeys := make(map[string]bool)
+		for _, k := range keysA {
+			allKeys[k] = true
+		}
+		for _, k := range keysB {
+			allKeys[k] = true
+		}
+
+		sortedKeys := make([]string, 0, len(allKeys))
+		for k := range allKeys {
+			sortedKeys = append(sortedKeys, k)
+		}
+		sort.Strings(sortedKeys)
+
+		for _, key := range sortedKeys {
+			subPath := key
+			if path != "" && path != "root" {
+				subPath = path + "." + key
+			} else if path == "root" {
+				subPath = key
+			}
+
+			valInA, inA := mapA[key]
+			valInB, inB := mapB[key]
+
+			if inA && !inB {
+				results = append(results, DiffResult{Type: DiffRemove, Path: subPath, OldValue: valInA})
+			} else if !inA && inB {
+				results = append(results, DiffResult{Type: DiffAdd, Path: subPath, NewValue: valInB})
+			} else {
+				results = append(results, compareJSON(subPath, valInA, valInB)...)
+			}
+		}
+
+	case reflect.Slice:
+		sliceA := a.([]interface{})
+		sliceB := b.([]interface{})
+
+		maxLen := len(sliceA)
+		if len(sliceB) > maxLen {
+			maxLen = len(sliceB)
+		}
+
+		for i := 0; i < maxLen; i++ {
+			subPath := fmt.Sprintf("%s[%d]", path, i)
+			if path == "root" {
+				subPath = fmt.Sprintf("[%d]", i)
+			}
+
+			if i < len(sliceA) && i >= len(sliceB) {
+				results = append(results, DiffResult{Type: DiffRemove, Path: subPath, OldValue: sliceA[i]})
+			} else if i >= len(sliceA) && i < len(sliceB) {
+				results = append(results, DiffResult{Type: DiffAdd, Path: subPath, NewValue: sliceB[i]})
+			} else {
+				results = append(results, compareJSON(subPath, sliceA[i], sliceB[i])...)
+			}
+		}
+
+	default:
+		if !reflect.DeepEqual(a, b) {
+			results = append(results, DiffResult{Type: DiffModify, Path: path, OldValue: a, NewValue: b})
+		}
+	}
+
+	return results
+}
+
+func getKeys(m map[string]interface{}) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func formatJSONFast(v interface{}) (string, error) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// --- Format Conversion Engine ---
+
+func convertFormat(obj interface{}, format string) ([]byte, error) {
+	switch format {
+	case "yaml", "yml":
+		var buf bytes.Buffer
+		enc := yaml.NewEncoder(&buf)
+		enc.SetIndent(2)
+		err := enc.Encode(obj)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert to YAML: %w", err)
+		}
+		return buf.Bytes(), nil
+	case "toml":
+		b, err := toml.Marshal(obj)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert to TOML: %w", err)
+		}
+		return b, nil
+	default:
+		return nil, fmt.Errorf("unsupported format '%s', available formats: yaml, toml", format)
+	}
+}
+
+// --- Masking Engine ---
+
+func applyMask(obj interface{}, maskKeysStr string) interface{} {
+	if maskKeysStr == "" || obj == nil {
+		return obj
+	}
+
+	keysToMask := strings.Split(maskKeysStr, ",")
+	maskMap := make(map[string]bool)
+	for _, k := range keysToMask {
+		maskMap[strings.TrimSpace(k)] = true
+	}
+
+	return maskRecursive(obj, maskMap)
+}
+
+func maskRecursive(v interface{}, maskMap map[string]bool) interface{} {
+	switch val := v.(type) {
+	case map[string]interface{}:
+		newMap := make(map[string]interface{})
+		for k, child := range val {
+			if maskMap[k] {
+				newMap[k] = "***"
+			} else {
+				newMap[k] = maskRecursive(child, maskMap)
+			}
+		}
+		return newMap
+	case []interface{}:
+		newArr := make([]interface{}, len(val))
+		for i, child := range val {
+			newArr[i] = maskRecursive(child, maskMap)
+		}
+		return newArr
+	}
+	return v
+}
+
+// --- Schema Engine ---
+
+func validateSchema(jsonData []byte, schemaFile string) error {
+	schemaLoader := gojsonschema.NewReferenceLoader("file://" + schemaFile)
+	documentLoader := gojsonschema.NewBytesLoader(jsonData)
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		return fmt.Errorf("failed to load schema: %w", err)
+	}
+
+	if result.Valid() {
+		return nil
+	}
+
+	var errMsgs []string
+	for _, desc := range result.Errors() {
+		errMsgs = append(errMsgs, fmt.Sprintf("- %s", desc))
+	}
+	return fmt.Errorf("schema validation failed:\n%s", stringsJoin(errMsgs, "\n"))
+}
+
+func stringsJoin(arr []string, sep string) string {
+	res := ""
+	for i, s := range arr {
+		if i > 0 {
+			res += sep
+		}
+		res += s
+	}
+	return res
+}
+
+// --- Clipboard Engine ---
+
+func copyToClipboard(text string) error {
+	err := clipboard.WriteAll(text)
+	if err != nil {
+		return fmt.Errorf("failed to copy to clipboard: %w", err)
+	}
+	return nil
+}
+
+// --- TUI Engine ---
+
+func runInteractiveTUI(jsonData []byte) error {
+	var jsonObj interface{}
+	if err := json.Unmarshal(jsonData, &jsonObj); err != nil {
+		return fmt.Errorf("invalid JSON for TUI: %w", err)
+	}
+
+	app := tview.NewApplication()
+
+	tree := tview.NewTreeView().
+		SetRoot(tview.NewTreeNode("root").SetColor(tcell.ColorRed)).
+		SetCurrentNode(tview.NewTreeNode("root"))
+
+	addNode(tree.GetRoot(), jsonObj)
+
+	tree.SetSelectedFunc(func(node *tview.TreeNode) {
+		reference := node.GetReference()
+		if reference == nil {
+			return // Selecting the root node does nothing.
+		}
+		children := node.GetChildren()
+		if len(children) == 0 {
+			// Load and show children on first selection.
+			addNode(node, reference)
+		} else {
+			// Collapse if visible, expand if collapsed.
+			node.SetExpanded(!node.IsExpanded())
+		}
+	})
+
+	layout := tview.NewFlex().
+		AddItem(tree, 0, 1, true)
+
+	app.SetRoot(layout, true)
+
+	if err := app.Run(); err != nil {
+		return fmt.Errorf("error running TUI: %w", err)
+	}
+
+	return nil
+}
+
+func addNode(target *tview.TreeNode, value interface{}) {
+	switch v := value.(type) {
+	case map[string]interface{}:
+		for k, val := range v {
+			node := tview.NewTreeNode(k).
+				SetReference(val).
+				SetSelectable(true).
+				SetColor(tcell.ColorAqua)
+
+			if isLeaf(val) {
+				node.SetText(fmt.Sprintf("%s: %v", k, formatLeaf(val)))
+				node.SetColor(tcell.ColorGreen)
+			}
+			target.AddChild(node)
+		}
+	case []interface{}:
+		for i, val := range v {
+			node := tview.NewTreeNode(fmt.Sprintf("[%d]", i)).
+				SetReference(val).
+				SetSelectable(true).
+				SetColor(tcell.ColorYellow)
+
+			if isLeaf(val) {
+				node.SetText(fmt.Sprintf("[%d]: %v", i, formatLeaf(val)))
+				node.SetColor(tcell.ColorGreen)
+			}
+			target.AddChild(node)
+		}
+	}
+}
+
+func isLeaf(value interface{}) bool {
+	switch value.(type) {
+	case map[string]interface{}, []interface{}:
+		return false
+	default:
+		return true
+	}
+}
+
+func formatLeaf(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		return fmt.Sprintf("\"%s\"", v)
+	case nil:
+		return "null"
+	default:
+		return fmt.Sprintf("%v", v)
+	}
 }
